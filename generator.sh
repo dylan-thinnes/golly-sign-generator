02f49e2d21e26efe04232204656fac8e1eddda75
Dylan Thinnes <dylan.thinnes@protonmail.com>
2023-01-04 21:56:16 +0000

    remove unused generator.sh

--- generator.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-golly generator.lua
-gcc ./rle-to-ppm.c -o rle-to-ppm
-cat red.rle   | ./rle-to-ppm > red.ppm
-cat green.rle | ./rle-to-ppm > green.ppm
-cat blue.rle  | ./rle-to-ppm > blue.ppm
-python3 merge.py
